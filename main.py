02cb75c75a0665898668831b735894b82e99add0
AmburgD <89587758+AmburgD@users.noreply.github.com>
2023-01-13 14:08:38 -0500

    test push to check if this works

---
+++ main.py
@@ -0,0 +1,6 @@
+firstNumber = input("what is the first number?")
+secondNumber = input("what is the second number?")
+thirdNumber = int(firstNumber) + int(secondNumber)
+print(thirdNumber)
+myName = "dax"
+print(myName.capitalize())
